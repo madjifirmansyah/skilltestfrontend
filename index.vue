582e7ed6ec06da9caaf36415fdb6f18a66f9289c
Mochammad Adji Firmansyah <79091568+madjifirmansyah@users.noreply.github.com>
2021-09-16 21:51:00 +0700

    skill test front end

---
+++ index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" v-model="item" @keypress.enter="add">
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item}}
+        <button @click="remove(index)">-</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      items: [],
+      item:''
+    }
+  },
+methods: {
+  add(item){
+    this.items.push(this.item)
+  },
+  remove(index){
+    this.items.splice(index, 1)
+    }
+  },
+}
+</script>
+
